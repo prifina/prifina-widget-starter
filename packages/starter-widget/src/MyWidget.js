536eae04ad47435cbd646b87915743c85f4d9aca
hamza <hamzahalilovic.12@gmail.com>
2022-02-02 21:42:28 +0100

    s

--- packages/starter-widget/src/MyWidget.js
+++ packages/starter-widget/src/MyWidget.js
@@ -5,7 +5,7 @@ import { usePrifina, Op } from "@prifina/hooks";
 // unique appID for the app....
 const appID = "1u3f465t4cNSWYiyKFVwBG";
 
-const App = () => {
+const MyWidget = () => {
   // init hook and get provider api services...
 
   return (
@@ -15,4 +15,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default MyWidget;
